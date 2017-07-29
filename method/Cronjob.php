ceb4afa60cab7d90324aba959116d05c427fb222
gizmore <gizmore@wechall.net>
2017-07-29 02:33:59 +0200

    backup-working

--- method/Cronjob.php
+++ method/Cronjob.php
@@ -14,8 +14,68 @@ final class Backup_Cronjob extends GWF_MethodCronjob
 		}
 	}
 	
+	private function tempDir()
+	{
+	    return GWF_PATH . 'temp/backup/';
+	}
+	
 	private function doBackup()
 	{
-		
+	    GWF_File::removeDir($this->tempDir());
+	    GWF_File::createDir($this->tempDir());
+	    $this->doMysqlDump();
+	    $this->doFilesDump();
+	    $this->createArchive();
+	    GWF_File::removeDir($this->tempDir());
 	}
-}
+	
+	private function doMysqlDump()
+	{
+	    $username = GWF_DB_USER;
+	    $password = GWF_DB_PASS;
+	    $database = GWF_DB_NAME;
+	    $sitename = $this->getSiteName();
+	    $today = date('Ymd');
+	    $path = $this->tempDir() . "$sitename.$today.sql.gz";
+	    $command = "mysqldump -u $username -p$password $database | gzip > $path";
+	    $output = null; $return_val = null;
+	    exec($command, $output, $return_val);
+	    if ($return_val !== 0)
+	    {
+	        $this->logError("Could not create sql backup");
+	    }
+	}
+
+	private function doFilesDump()
+	{
+	    $src = GWF_PATH . 'dbimg/files';
+	    $sitename = $this->getSiteName();
+	    $today = date('Ymd');
+	    $path = $this->tempDir() . "$sitename.$today.files.zip";
+	    $command = "zip -r9 $path $src";
+	    $output = null; $return_val = null;
+	    exec($command, $output, $return_val);
+	    if ($return_val !== 0)
+	    {
+	        $this->logError("Could not create files backup");
+	    }
+	}
+
+	private function createArchive()
+	{
+	    $src = $this->tempDir();
+	    $backupPath = GWF_PATH . "protected/backup/";
+	    $sitename = $this->getSiteName();
+	    $today = date('Ymd');
+	    GWF_File::createDir($backupPath);
+	    $path = "$backupPath$sitename.$today.zip";
+	    $command = "zip -r0 $path $src";
+	    $output = null; $return_val = null;
+	    exec($command, $output, $return_val);
+	    if ($return_val !== 0)
+	    {
+	        $this->logError("Could not create archive");
+	    }
+	    
+	}
+}	
